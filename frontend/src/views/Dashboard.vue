a1bb9c7f51c08d0a40cfd2cc4199a8358d828247
Rashid <rashid.hmaidan@agilelab.it>
2025-05-09 20:58:04 +0200

    added request to backend for switching acts

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -11,7 +11,7 @@ export default {
   data() {
     return {
       serverResponseCentral: '',
-      serverResponseSingles: [],
+      serverResponseSingles: [null],
       actuators: [],
       currentMode: 'individual', // NEW: active configuration display
       sensorsID: [],
@@ -111,7 +111,7 @@ export default {
           stateDesired
         });
 
-        this.serverResponseSingles[id] = response.data.receivedData.stateDesired ? 'Turned ON' : 'Turned OFF';
+        this.serverResponseSingles[id] = response.data.receivedData.stateDesired;
         this.currentMode = 'individual';
       } catch (error) {
         console.error('Error sending data:', error);
@@ -136,126 +136,90 @@ export default {
 
 
 <template>
-  <div class="flex flex-col">
-    <div class="card">
-      <div class="text-2xl font-semibold text-slate-800">Locals Data</div>
-      <hr class="border-t border-slate-300 my-6" />
-
-      <div v-if="isLoading" class="space-y-3">
-        <Skeleton width="50%" height="20px" />
+  <div class="card space-y-6">
+    <div class="flex justify-between items-center w-full">
+      <h1 class="text-2xl font-semibold text-slate-800">Locals Data</h1>
+      <div class="flex space-x-4"> <!-- Added space between the buttons -->
+        <button @click="setAllActuators(true)"
+          class="bg-blue-600 hover:bg-blue-700 text-white font-medium px-4 py-2 rounded-lg shadow transition">
+          Attiva Tutti
+        </button>
+        <button @click="setAllActuators(false)"
+          class="bg-blue-600 hover:bg-blue-700 text-white font-medium px-4 py-2 rounded-lg shadow transition">
+          Disattiva tutti
+        </button>
       </div>
+    </div>
 
-      <div v-else-if="sensorReg === true && localsData.length > 0" class="grid grid-cols-12 gap-6">
-        <div v-for="(item, index) in localsData" :key="index" class="col-span-12 md:col-span-6">
-          <div
-            class="bg-slate-50 border rounded-xl p-6 shadow-md h-full hover:scale-[1.01] transition-all duration-200 col-span-12 md:col-span-6 space-y-4">
+    <hr class="border-t border-slate-300 my-6" />
 
-            <!-- Room Name Centered -->
-            <h2 class="text-2xl font-bold text-center text-slate-800">{{ item.location }}</h2>
+    <div v-if="isLoading" class="space-y-3">
+      <Skeleton width="50%" height="20px" />
+    </div>
 
-            <!-- Sensor and Actuator Cards Side by Side -->
-            <div class="flex flex-col md:flex-row mt-4 items-center">
+    <div v-else-if="sensorReg === true && localsData.length > 0" class="grid grid-cols-12 gap-6">
+      <div v-for="(item, index) in localsData" :key="index" class="col-span-12 md:col-span-6">
+        <div
+          class="bg-slate-50 border rounded-xl p-6 shadow-md h-full hover:scale-[1.01] transition-all duration-200 col-span-12 md:col-span-6 space-y-4">
 
-              <!-- SensorCard -->
-              <SensorCard :name="item.sensorName" :temperature="item.temperature" :humidity="item.humidity"
-                :location="item.location" :disconnected="item.sensorDisconnected" />
+          <!-- Room Name Centered -->
+          <h2 class="text-2xl font-bold text-center text-slate-800">{{ item.location }}</h2>
 
-              <!-- Vertical separator -->
-              <div class="w-px h-16 bg-gray-200 mx-2"></div>
+          <!-- Sensor and Actuator Cards Side by Side -->
+          <div class="flex flex-col md:flex-row mt-4 items-center">
 
-              <!-- Actuator Card -->
-              <div class="flex-1">
-                <div v-if="item.actuatorStatus !== null">
-                  <div class="rounded-xl p-4 text-center h-full transition-all"
-                    :class="item.actuatorStatus ? 'border-2 border-red-500' : 'border-2 border-slate-50'">
-                    <!-- Actuator Name -->
-                    <h2 class="text-xl font-semibold text-slate-800 mb-4">{{ item.actuatorName }}</h2>
+            <!-- SensorCard -->
+            <SensorCard :name="item.sensorName" :temperature="item.temperature" :humidity="item.humidity"
+              :location="item.location" :disconnected="item.sensorDisconnected" />
 
-                    <!-- Status Text -->
-                    <div class="mb-4">
-                      <i class="pi pi-bolt text-2xl text-slate-800"></i>
-                      <span :class="['text-xl font-bold ml-2', item.actuatorStatus ? 'text-red-600' : 'text-gray-400']">
-                        {{ item.actuatorStatus ? 'ON' : 'OFF' }}
-                      </span>
-                    </div>
+            <!-- Vertical separator -->
+            <div class="w-px h-16 bg-gray-200 mx-2"></div>
 
-                    <!--  Circular Switch -->
-                    <div class="flex items-center justify-center gap-4">
+            <!-- Actuator Card -->
+            <div class="flex-1">
+              <div v-if="item.actuatorStatus !== null">
+                <div class="rounded-xl p-4 text-center h-full transition-all"
+                  :class="item.actuatorStatus ? 'border-2 border-red-500' : 'border-2 border-slate-50'">
+                  <!-- Actuator Name -->
+                  <h2 class="text-xl font-semibold text-slate-800 mb-4">{{ item.actuatorName }}</h2>
 
-                      <label @click="setSingleActuator(item.actuatorName, !item.actuatorStatus)"
-                        class="relative w-16 h-10">
-                        <input type="checkbox" v-model="item.actuatorStatus" class="sr-only" />
-                        <div :class="[
-                          'w-full h-full flex items-center justify-center rounded-full border transition-colors',
-                          item.actuatorStatus
-                            ? 'bg-red-500 text-white border-red-600'
-                            : 'bg-gray-100 text-slate-700 border-gray-300'
-                        ]">
-                          <i class="pi pi-power-off text-xl"></i>
-                        </div>
-                      </label>
-                    </div>
+                  <!-- Status Text -->
+                  <div class="mb-4">
+                    <i class="pi pi-bolt text-2xl text-slate-800"></i>
+                    <span :class="['text-xl font-bold ml-2', item.actuatorStatus ? 'text-red-600' : 'text-gray-400']">
+                      {{ item.actuatorStatus ? 'ON' : 'OFF' }}
+                    </span>
                   </div>
-                </div>
 
-                <div v-else>
-                  <span class="text-gray-500 ml-6">Nessun attuatore associato</span>
+                  <!--  Circular Switch -->
+                  <div class="flex items-center justify-center gap-4">
+
+                    <label class="relative w-16 h-10">
+                      <input type="checkbox" v-model="item.actuatorStatus"
+                        @change="setSingleActuator(item.actuatorName, item.actuatorStatus)" class="sr-only" />
+                      <div :class="[
+                        'w-full h-full flex items-center justify-center rounded-full border transition-colors',
+                        serverResponseSingles[item.actuatorName]
+                          ? 'bg-red-500 text-white border-red-600'
+                          : 'bg-gray-100 text-slate-700 border-gray-300'
+                      ]">
+                        <i class="pi pi-power-off text-xl"></i>
+                      </div>
+                    </label>
+                  </div>
                 </div>
               </div>
 
+              <div v-else>
+                <span class="text-gray-500 ml-6">Nessun attuatore associato</span>
+              </div>
             </div>
 
           </div>
+
         </div>
       </div>
-      <div v-else-if="sensorReg === false" class="text-gray-500">Nessun sensore registrato</div>
     </div>
+    <div v-else-if="sensorReg === false" class="text-gray-500">Nessun sensore registrato</div>
   </div>
 </template>
-
-<!-- 
-<template>
-  <div class="flex flex-col">
-    <div class="card">
-      <div class="text-2xl font-semibold text-slate-800">Sensors Data</div>
-      <hr class="border-t border-slate-300 my-6" />
-
-      <div v-if="isLoading" class="space-y-3">
-        <Skeleton width="50%" height="20px" />
-      </div>
-
-      <div v-else-if="localsData.length > 0" class="grid grid-cols-12 gap-8">
-        <div v-for="(item, index) in localsData" :key="index" class="card">
-          <SensorCard :name="item.sensorName" :temperature="item.temperature" :humidity="item.humidity"
-            :location="item.location" :disconnected="item.sensorDisconnected" />
-          <p class="mt-2">
-            🔌 Attuatore:
-            <strong v-if="item.actuatorStatus !== null">{{ item.actuatorStatus ? 'Attivo' : 'Spento' }}</strong>
-            <span v-else class="text-gray-500">Nessun attuatore associato</span>
-          </p>
-        </div>
-      </div>
-      <div v-else class="text-gray-500">
-        Nessun dato disponibile per sensori o attuatori.
-      </div>
-      <div v-if="actReg === true" class="grid grid-cols-12 gap-8">
-        ciao ci sono
-      </div>
-    </div>
-  </div>
-</template> -->
-
-<!-- <div v-for="sensor in sensors" :key="sensor.name" class="card p-4 mb-4 border border-slate-200 rounded-xl">
-  <h2 class="text-lg font-semibold">{{ sensor.name }}</h2>
-  <p>🌡️ Temperatura: {{ sensor.temperature }}°C</p>
-
-  <div v-if="actuatorMap[sensor.location]">
-    <p>🔌 Attuatore: {{ actuatorMap[sensor.location].status ? 'Attivo' : 'Spento' }}</p>
-  </div>
-</div>
-<div v-if="sensorReg === true" class="grid grid-cols-12 gap-8">
-        <SensorCard v-for="(sensAct, index) in localsData" :key="index" :name="sensAct.name"
-          :temperature="sensAct.temperature" :humidity="sensAct.humidity" :location="sensAct.location"
-          :disconnected="sensAct.disconnected" />
-
-      </div>  -->
